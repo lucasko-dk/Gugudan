73889aa01b11b70d71665e0de97fe389be988f62
Lucas ko <Lucas ko@DESKTOP-9M1BHF9>
2021-03-05 22:20:14 +0900

    구구단 수정

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,19 +1,27 @@
 
 public class Gugudan {
 	
-	public static int[] calculate(int times) {
-		int[] result = new int[9];
-		for(int i=0;i<result.length;i++) {
-			result[i]=times*(i+1);
-		}
-		return result;
-		
-	}
+//	public static int[] calculate(int times) {
+//		int[] result = new int[9];
+//		for(int i=0;i<result.length;i++) {
+//			result[i]=times*(i+1);
+//		}
+//		return result;
+//		
+//	}
 	
 	public static int[] front(int times,int times1) {
 		int[] result = new int[times1];
 			for(int i=0;i<result.length;i++) {
 				result[i]=times;
+			}
+		return result;
+	}	
+	
+	public static int[] calculate1(int times, int times1) {
+		int[] result = new int[times1];
+			for(int i=0;i<result.length;i++) {
+				result[i]=times*(i+1);
 			}
 		return result;
 	}	
@@ -26,13 +34,7 @@ public class Gugudan {
 		return result;
 	}	
 	
-	public static int[] calculate1(int times, int times1) {
-		int[] result = new int[times1];
-			for(int i=0;i<result.length;i++) {
-				result[i]=times*(i+1);
-			}
-		return result;
-	}	
+
 	
 	public static void print(int[] result0, int[] result, int[] result1) {
 		for(int i=0;i<result.length;i++) {
--- src/GugudanMain.java
+++ src/GugudanMain.java
@@ -6,19 +6,50 @@ public class GugudanMain {
 		
 		System.out.println("몇단 ? : " );
 		Scanner scanner = new Scanner(System.in);
-//		int InputValue = scanner.nextInt();
 		String InputValue = scanner.nextLine();
-		int InputValue1 = Integer.parseInt(InputValue);
-		int InputValue2 = 2;
-		int InputValue3 = Integer.parseInt(InputValue);
-//		int[] result = Gugudan.calculate(InputValue2);
-		if (InputValue1 >= 2) {  
-			while(InputValue2<=InputValue1) {
-				int[] result0 = Gugudan.front(InputValue2, InputValue3);
-				int[] result = Gugudan.calculate1(InputValue2, InputValue3);
-				int[] result1 = Gugudan.end(InputValue3);
+		String[] splitvalue = InputValue.split(",");
+		
+		int first = Integer.parseInt(splitvalue[0]);
+		int second = Integer.parseInt(splitvalue[1]);
+//	    System.out.println(first + " / ");
+//	    System.out.println(second);
+	    
+		//int InputValue1 = Integer.parseInt(InputValue);
+		int startno = 2;
+//		int InputValue3 = Integer.parseInt(InputValue);
+		
+		if (first >= 2) {  
+			while(startno<=first) {
+				int[] result0 = Gugudan.front(startno, second);  // 앞의숫자
+				int[] result1 = Gugudan.end(second); // 뒤의숫자 
+				int[] result = Gugudan.calculate1(startno, second); // 결과값
+//				System.out.println(result0[0]);
+//				System.out.println(result0[1]);
+//				System.out.println(result0[2]);
+//				System.out.println(result0[3]);
+//				System.out.println(result0[4]);
+//				System.out.println(result0[5]);
+//				System.out.println(result0[6]);
+//				
+//				System.out.println(result1[0]);
+//				System.out.println(result1[1]);
+//				System.out.println(result1[2]);
+//				System.out.println(result1[3]);
+//				System.out.println(result1[4]);
+//				System.out.println(result1[5]);
+//				System.out.println(result1[6]);
+//				
+//				
+//				System.out.println(result[0]);
+//				System.out.println(result[1]);
+//				System.out.println(result[2]);
+//				System.out.println(result[3]);
+//				System.out.println(result[4]);
+//				System.out.println(result[5]);
+//				System.out.println(result[6]);
+				
 				Gugudan.print(result0,result,result1);
-				InputValue2=InputValue2+1;  
+				startno=startno+1;  
 			}  
 		} else {
 			System.out.println("input numer greater than 1");
